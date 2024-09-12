a4fc1b12901fb50828be6971fc13fd980bbee1c8
Akira Yano <akira@enpowered.com>
2024-09-12 14:03:56 -0400

    fix: build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useEffect } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// import outputs from "../amplify_outputs.json";
 console.log({
   userPoolId: import.meta.env.VITE_AWS_COGNITO_USER_POOL_ID,
   userPoolClientId: import.meta.env.VITE_AWS_COGNITO_CLIENT_ID,
